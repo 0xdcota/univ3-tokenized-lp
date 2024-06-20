b72183f392e37750d9ed2a6264d38cd314f6f27b
dcota <32775237+DaigaroCota@users.noreply.github.com>
2024-06-20 13:51:30 -0700

    chore: tbd addresses in pm2 config file

--- script/pm2/pm2-ecosystem.config.js
+++ script/pm2/pm2-ecosystem.config.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
-const XOC_ADDR = "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984";
-const LP_ADDRESS_BASE = "0x8250f4aF4B972684F7b336503E2D6dFeDeB1487a";
+const XOC_ADDR = "";
+const LP_ADDRESS_BASE = "";
 
 const PK = process.env.PRIVATE_KEY;
 const BASE_RPC = process.env.OPTIMISM_RPC;
